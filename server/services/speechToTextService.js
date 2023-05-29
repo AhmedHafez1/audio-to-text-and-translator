51bc1ecbf779ea05ba491964e21ad62312ca3e2a
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-29 18:03:29 +0300

    speech to text service

---
+++ server/services/speechToTextService.js
@@ -0,0 +1,32 @@
+const speech = require("@google-cloud/speech");
+const client = speech.SpeechClient();
+
+async function transcribeAudio(
+  audioBuffer,
+  encoding,
+  sampleRateHertz,
+  languageCode
+) {
+  const request = {
+    audio: {
+      content: audioBuffer.toString("base64"),
+    },
+    config: {
+      encoding,
+      sampleRateHertz,
+      languageCode,
+    },
+  };
+
+  try {
+    const [response] = await client.recognize(request);
+    const transcription = response.results
+      .map((result) => result.alternatives[0].transcript)
+      .join("\n");
+    return transcription;
+  } catch (error) {
+    console.error("Error transcribing audio:", error);
+  }
+}
+
+module.exports = transcribeAudio;
