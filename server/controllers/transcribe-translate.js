d5b98530fda3c0aba972cba858bd884b0100c4cb
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-29 19:19:20 +0300

    finishing BE implementation

---
+++ server/controllers/transcribe-translate.js
@@ -0,0 +1,41 @@
+const { transcribeAudio } = require("./services/speechToTextService");
+const {
+  detectLanguage,
+  translateText,
+} = require("./services/translationService");
+
+async function transcribeTranslate(req, res) {
+  try {
+    const {
+      audioBuffer,
+      encoding,
+      sampleRateHertz,
+      languageCode,
+      targetLanguage,
+    } = req.body;
+    const buffer = Buffer.from(audioBuffer, "base64");
+
+    // Transcribe the audio
+    const transcription = await transcribeAudio(
+      buffer,
+      encoding,
+      sampleRateHertz,
+      languageCode
+    );
+
+    // Detect the language of the transcription
+    const detectedLanguage = await detectLanguage(transcription);
+
+    // Translate the transcription to the target language
+    const translation =
+      detectLanguage !== targetLanguage
+        ? await translateText(transcription, targetLanguage)
+        : transcription;
+
+    res.json({ transcription, translation });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+module.exports = transcribeTranslate;
