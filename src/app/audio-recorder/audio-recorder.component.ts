42b66c156d7978461fe139afacd8124ba140c535
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2023-05-30 12:12:33 +0300

    added AudioTranscriptionService to FE

---
+++ src/app/audio-recorder/audio-recorder.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-recorder',
+  templateUrl: './audio-recorder.component.html',
+  styleUrls: ['./audio-recorder.component.css'],
+})
+export class AudioRecorderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
