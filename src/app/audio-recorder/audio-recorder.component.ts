e13fcbc8a1a98ba8cd202db48bd67d02654cee56
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2023-05-30 13:41:13 +0300

    startRecording method

--- src/app/audio-recorder/audio-recorder.component.ts
+++ src/app/audio-recorder/audio-recorder.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as RecordRTC from 'recordrtc';
+import { AudioTranscriptionService } from '../audio-transcription.service';
 
 @Component({
   selector: 'app-audio-recorder',
@@ -6,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./audio-recorder.component.css'],
 })
 export class AudioRecorderComponent implements OnInit {
-  constructor() {}
+  isRecording = false;
+  transcription: string | null = null;
+  translation: string | null = null;
+  private recordRTC: RecordRTC | null = null;
+
+  constructor(private audioTranscriptionService: AudioTranscriptionService) {}
 
   ngOnInit(): void {}
+
+  startRecording(): void {
+    this.isRecording = true;
+    this.transcription = null;
+    this.translation = null;
+
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        this.recordRTC = new RecordRTC(stream, {
+          type: 'audio',
+          mimeType: 'audio/wav',
+          recorderType: RecordRTC.StereoAudioRecorder,
+          numberOfAudioChannels: 1,
+        });
+        this.recordRTC.startRecording();
+      })
+      .catch((error) => {
+        console.error('Error starting recording:', error);
+      });
+  }
 }
