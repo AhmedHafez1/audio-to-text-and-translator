a556cc4e3cb660f56a0ba373b074c8460f6887a9
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-06-20 05:12:16 +0300

    auth, login and register components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TranscriptionComponent } from './transcription/transcription.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
   { path: 'transcription', component: TranscriptionComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: 'transcription', pathMatch: 'full' },
 ];
 
