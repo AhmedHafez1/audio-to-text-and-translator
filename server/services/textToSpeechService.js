2ff081b7664ade4ee0fcc7934f6fa0cfebd47022
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-07-03 07:58:53 +0300

    synthesize Speech in different languages

--- server/services/textToSpeechService.js
+++ server/services/textToSpeechService.js
@@ -4,17 +4,13 @@ const client = new textToSpeech.TextToSpeechClient();
 exports.synthesizeSpeech = async (text, languageCode) => {
   const request = {
     audioConfig: {
-      audioEncoding: "LINEAR16",
-      effectsProfileId: ["small-bluetooth-speaker-class-device"],
-      pitch: 0,
-      speakingRate: 1,
+      audioEncoding: "MP3",
     },
     input: {
-      text: "Google Cloud Text-to-Speech enables developers to synthesize natural-sounding speech with 100+ voices, available in multiple languages and variants. It applies DeepMind’s groundbreaking research in WaveNet and Google’s powerful neural networks to deliver the highest fidelity possible. As an easy-to-use API, you can create lifelike interactions with your users, across many applications and devices.",
+      text,
     },
     voice: {
-      languageCode: "en-US",
-      name: "en-US-Neural2-J",
+      languageCode,
     },
   };
 
