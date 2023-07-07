589da4f39cf9feb255f4a5fc8d49e182d2663254
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-07-07 07:01:30 +0300

    Toolbar, Nav, Logo, Logout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
 import { SharedModule } from '../shared/shared.module';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ErrorDialogComponent, ToolbarComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, RouterModule.forChild([])],
   exports: [ToolbarComponent],
 })
 export class CoreModule {}
