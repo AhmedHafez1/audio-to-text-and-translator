20ce5cc2531506a3d523017094261286a7d8b946
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-07-11 07:53:28 +0300

    Main Entry Page

--- src/app/main-entry/main-entry.component.ts
+++ src/app/main-entry/main-entry.component.ts
@@ -8,51 +8,62 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 export class MainEntryComponent {
   customOptions: OwlOptions = {
     loop: true,
-    autoplay: true,
+    autoplay: false,
     mouseDrag: true,
     touchDrag: true,
     pullDrag: true,
     dots: true,
     navSpeed: 700,
-    navText: ['<=', '=>'],
+    navText: ['', ''],
     responsive: {
       0: {
         items: 1,
       },
       400: {
-        items: 2,
+        items: 1,
       },
       740: {
-        items: 3,
+        items: 1,
       },
       940: {
-        items: 4,
+        items: 1,
       },
     },
     nav: true,
   };
 
-  slidesStore = [
+  slides = [
     {
       id: '1',
-      src: '../../assets/images/spacex-TV2gg2kZD1o-unsplash.jpg',
+      src: '../../assets/images/kid.jpg',
       alt: 'Beautiful sunset over the ocean',
       title: 'Sunset at the Beach',
-      width: 500,
+      description:
+        'Access your previous transcriptions anytime and anywhere with our cloud storage.',
     },
     {
       id: '2',
-      src: '../../assets/images/Speech.avif',
+      src: '../../assets/images/mic.jpg',
       alt: 'Lush green forest with a flowing river',
       title: 'Serene Forest Retreat',
-      width: 500,
+      description:
+        'Use our editor to make corrections, add punctuation, format text, and more.',
     },
     {
       id: '3',
-      src: '../../assets/images/spacex-TV2gg2kZD1o-unsplash.jpg',
+      src: '../../assets/images/sound.jpg',
       alt: 'City skyline at night with colorful lights',
       title: 'Urban Nightscape',
-      width: 500,
+      description:
+        'Type or paste any text and listen to it in a variety of languages and voices.',
+    },
+    {
+      id: '4',
+      src: '../../assets/images/mic.jpg',
+      alt: 'Lush green forest with a flowing river',
+      title: 'Serene Forest Retreat',
+      description:
+        'Use your microphone or upload an audio file and get a transcription in seconds.',
     },
   ];
 }
